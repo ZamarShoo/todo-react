7b0c88107ee3b698b7c6ae6b7fc034cc9bb97aea
Ivan Shulga <shg.ivan97@gmail.com>
2020-06-07 16:29:14 +0300

    Updates

---
+++ precache-manifest.bf9be8c48c5a62d45bcbce71fe6d5e12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ccca1f22f82df2bcf4475d8f038818",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "ebf5ef380c463508bb97",
+    "url": "/react-todo/static/css/main.c92f0ce6.chunk.css"
+  },
+  {
+    "revision": "939bf3ef8229f49867e9",
+    "url": "/react-todo/static/js/2.7350ac3a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo/static/js/2.7350ac3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf5ef380c463508bb97",
+    "url": "/react-todo/static/js/main.9d38228b.chunk.js"
+  },
+  {
+    "revision": "c4a624bc38d60c6d0b32",
+    "url": "/react-todo/static/js/runtime-main.b2438576.js"
+  },
+  {
+    "revision": "384523507dbdb3c9c6188c3b1104d0bf",
+    "url": "/react-todo/static/media/delete-icon-dark.38452350.svg"
+  },
+  {
+    "revision": "53935243875a2e3a42979c097d48e34a",
+    "url": "/react-todo/static/media/delete-icon.53935243.svg"
+  }
+]);
